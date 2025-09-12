c5c13a78f4f5b56cae32994f3e692cbe35a88600
OK <152398215+ASSI4YANN@users.noreply.github.com>
2025-09-12 20:12:48 +0000

    Update MainActivity.kt
    modif

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -203,11 +203,7 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  */
-/*private*/ internal fun calculateTip(
-    amount: Double,
-    tipPercent: Double /*= 15.0*/,
-    roundUp: Boolean
-): String {
+private fun calculateTip(amount: Double,tipPercent: Double /*= 15.0*/,roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp) {
         tip = kotlin.math.ceil(tip)
